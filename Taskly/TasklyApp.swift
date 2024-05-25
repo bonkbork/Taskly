11a6e3bc9fd9b6291209d6a7d2655ecb81347dc1
Bryant Galvan Andraca <158379008+bonkbork@users.noreply.github.com>
2024-05-24 23:26:37 -0700

    Initial Commit

---
+++ Taskly/TasklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasklyApp.swift
+//  Taskly
+//
+//  Created by Bryant Galvan on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct TasklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
